79a320f7889353ab651e1ef3f726d82ee39d5c79
SaTr0V <alexander1308@inbox.ru>
2026-02-08 00:49:02 +0300

    Add DIV2K and FFHQ datasets' sample images

--- project/src/service/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-* A FastAPI service for image uploading.
-
-You can run it from the project's directory using following command (ignore exclamation mark):
-! uv run uvicorn src.service.images:app --reload
-"""
-
-__version__ = "0.1.0"

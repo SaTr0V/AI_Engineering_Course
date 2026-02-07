f2589dba7d84045f4a3332f8bf603a749f39fb35
SaTr0V <alexander1308@inbox.ru>
2026-02-08 00:50:28 +0300

    Refactor directory name

---
+++ project/src/api/__init__.py
@@ -0,0 +1,8 @@
+"""
+* A FastAPI service for image uploading.
+
+You can run it from the project's directory using following command (ignore exclamation mark):
+! uv run uvicorn src.api.images:app --reload
+"""
+
+__version__ = "0.1.0"
